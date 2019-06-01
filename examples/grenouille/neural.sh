10fb7d219d5d1fc905836e59a0bc87827c774fed
BErnard Maltais <bernard@ducourier.com>
2019-06-01 09:19:55 -0400

    Update parameters

--- examples/grenouille/neural.sh
+++ examples/grenouille/neural.sh
@@ -25,10 +25,10 @@ th ../neural_style.lua -seed 100 \
 -style_layers relu3_1,relu4_1,relu4_2,relu5_1 \
 -tv_weight 0.000085 -original_colors 0 && rm *_*0.png
 
-th ../neural_style_tile.lua -seed 100 \
+th ../neural_style.lua -seed 100 \
 -backend cudnn -cudnn_autotune \
 -style_scale 1 -init image -normalize_gradients \
--image_size 1200 -tile_size 500 -num_iterations 500 -save_iter 1 \
+-image_size 1200 -tile_size 500 -num_iterations 500 -save_iter 50 \
 -content_weight 100 -style_weight 1000 \
 -style_image style.jpg \
 -content_image content.jpg \
